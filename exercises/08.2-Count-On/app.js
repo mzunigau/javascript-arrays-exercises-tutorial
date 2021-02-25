8450cb8c4ba402278762227e87ebcf6c7295276a
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-02-25 02:35:48 +0000

    ejercicio 11

--- exercises/08.2-Count-On/app.js
+++ exercises/08.2-Count-On/app.js
@@ -2,8 +2,12 @@ let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
 for(let index = 0; index < myArray.length; index++){
-    let element = myArray[index];
+    let element =typeof myArray[index];
+
     // MAGIC HAPPENS HERE
+    if(element == "object"){
+        hello.push(myArray[index]);
+    }
 }
 
-console.log(hello)
+console.log(hello);
--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -2,7 +2,12 @@ function sumTheElements(theArray){
 	
 	var total = 0;
 	
-	//your code here
+    //your code here
+    for (let index = 0; index < theArray.length; index++) {
+        total = theArray[index] + total;
+    }
 
 	return total;
 }
+
+console.log(sumTheElements([2,13,34,5]));
